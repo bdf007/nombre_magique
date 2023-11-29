34c68bb8056623dd5dfbc004f8d482ef9e110c17
bdf007 <bdf007@gmail.com>
2023-11-29 14:23:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,9 +5,41 @@ namespace nombre_magique
 {
     class Program
     {
+
+        static int DemanderNb(int min, int max)
+        {
+           
+            int nb = min-1;
+
+            while (nb < min || nb > max)
+            {
+                Console.Write($"Rentrez un nombre entre {min} et {max}: ");
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                   nb = int.Parse(reponse);
+                     if (nb < min || nb > max)
+                    {
+                          Console.WriteLine($"Le nombre doit être compris entre {min} et {max}");
+                     }
+                }
+                 catch
+                {
+                   Console.WriteLine("Erreur de saisie");
+                
+                 }
+            }
+            return nb;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            const int nbMin=1;
+            const int nbMax=10;
+            int nombre = DemanderNb(nbMin, nbMax);
+            Console.WriteLine($"votre nombre est {nombre}");
+
         }
     }
 }
