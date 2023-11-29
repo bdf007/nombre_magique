c3edae4cd9aa9ad5846b5fd0815c5387e80d0fba
bdf007 <bdf007@gmail.com>
2023-11-29 13:57:53 +0100

    initialisation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace nombre_magique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
